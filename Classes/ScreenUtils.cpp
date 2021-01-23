354bf9cc46913efe1dcf851bc57ee08c63fbef27
mehmetemin <kacmazemin72@gmail.com>
2021-01-23 13:23:25 +0300

    feat: added utils classes

---
+++ Classes/ScreenUtils.cpp
@@ -0,0 +1,76 @@
+//
+// Created by emin on 23.01.2021.
+//
+
+#include "ScreenUtils.h"
+
+USING_NS_CC;
+
+Rect ScreenUtils::s_visibleRect;
+
+void ScreenUtils::lazyInit()
+{
+    // no lazy init
+    // Useful if we change the resolution in runtime
+    s_visibleRect = Director::getInstance()->getOpenGLView()->getVisibleRect();
+}
+
+Rect ScreenUtils::getVisibleRect()
+{
+    lazyInit();
+    return s_visibleRect;
+}
+
+Vec2 ScreenUtils::left()
+{
+    lazyInit();
+    return Vec2(s_visibleRect.origin.x, s_visibleRect.origin.y+s_visibleRect.size.height/2);
+}
+
+Vec2 ScreenUtils::right()
+{
+    lazyInit();
+    return Vec2(s_visibleRect.origin.x+s_visibleRect.size.width, s_visibleRect.origin.y+s_visibleRect.size.height/2);
+}
+
+Vec2 ScreenUtils::top()
+{
+    lazyInit();
+    return Vec2(s_visibleRect.origin.x+s_visibleRect.size.width/2, s_visibleRect.origin.y+s_visibleRect.size.height);
+}
+
+Vec2 ScreenUtils::bottom()
+{
+    lazyInit();
+    return Vec2(s_visibleRect.origin.x+s_visibleRect.size.width/2, s_visibleRect.origin.y);
+}
+
+Vec2 ScreenUtils::center()
+{
+    lazyInit();
+    return Vec2(s_visibleRect.origin.x+s_visibleRect.size.width/2, s_visibleRect.origin.y+s_visibleRect.size.height/2);
+}
+
+Vec2 ScreenUtils::leftTop()
+{
+    lazyInit();
+    return Vec2(s_visibleRect.origin.x, s_visibleRect.origin.y+s_visibleRect.size.height);
+}
+
+Vec2 ScreenUtils::rightTop()
+{
+    lazyInit();
+    return Vec2(s_visibleRect.origin.x+s_visibleRect.size.width, s_visibleRect.origin.y+s_visibleRect.size.height);
+}
+
+Vec2 ScreenUtils::leftBottom()
+{
+    lazyInit();
+    return s_visibleRect.origin;
+}
+
+Vec2 ScreenUtils::rightBottom()
+{
+    lazyInit();
+    return Vec2(s_visibleRect.origin.x+s_visibleRect.size.width, s_visibleRect.origin.y);
+}
---
+++ Classes/ScreenUtils.h
@@ -0,0 +1,31 @@
+//
+// Created by emin on 23.01.2021.
+//
+
+#ifndef PROJ_ANDROID_SCREENUTILS_H
+#define PROJ_ANDROID_SCREENUTILS_H
+
+#include "cocos2d.h"
+
+
+class ScreenUtils {
+
+public:
+    static cocos2d::Rect getVisibleRect();
+
+    static cocos2d::Vec2 left();
+    static cocos2d::Vec2 right();
+    static cocos2d::Vec2 top();
+    static cocos2d::Vec2 bottom();
+    static cocos2d::Vec2 center();
+    static cocos2d::Vec2 leftTop();
+    static cocos2d::Vec2 rightTop();
+    static cocos2d::Vec2 leftBottom();
+    static cocos2d::Vec2 rightBottom();
+private:
+    static void lazyInit();
+    static cocos2d::Rect s_visibleRect;
+};
+
+
+#endif //PROJ_ANDROID_SCREENUTILS_H
