9b81ae59615659463ddac2041a5506982b1c03ed
mehmetemin <kacmazemin72@gmail.com>
2021-01-24 18:58:11 +0300

    feat: added boundaries and balls

--- Classes/GameLayer.cpp
+++ Classes/GameLayer.cpp
@@ -4,9 +4,8 @@
 
 #include "GameLayer.h"
 #include "Utils/ScreenUtils.h"
-#include "Box2D/Box2D.h"
-
-#define SCALE_RATIO 32
+#include "Physics/PhysicsManager.h"
+#include "GameComponents/Ball.h"
 
 cocos2d::Scene *GameLayer::createScene()
 {
@@ -15,95 +14,16 @@ cocos2d::Scene *GameLayer::createScene()
 
 bool GameLayer::init()
 {
-    // 1. super init first
     if ( !cocos2d::Scene::init() )
     {
         return false;
     }
 
-    auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
-    cocos2d::Vec2 origin = cocos2d::Director::getInstance()->getVisibleOrigin();
+    physicsManager = std::make_unique<PhysicsManager>();
 
-    auto label = cocos2d::Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    createBoundaries();
 
-    label->setPosition(cocos2d::Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    this->addChild(label, 1);
-
-    cocos2d::Sprite* sprite = cocos2d::Sprite::create("HelloWorld.png");
-    sprite->setAnchorPoint(cocos2d::Vec2::ANCHOR_TOP_LEFT);
-
-    sprite->setPosition(ScreenUtils::leftTop());
-
-    this->addChild(sprite, 0);
-
-
-    world = new b2World(gravity);
-
-    uint32 flags = 0;
-    flags += b2Draw::e_shapeBit;
-    flags += b2Draw::e_jointBit;
-    //flags +=b2Draw::e_aabbBit;
-    flags += b2Draw::e_centerOfMassBit;
-
-    if(!debugDraw)
-    {
-        debugDraw = new GLESDebugDraw(SCALE_RATIO);
-        debugDraw->AppendFlags(flags);
-    }
-
-    world->SetDebugDraw(debugDraw);
-
-    {
-        cocos2d::Vec2 pos(cocos2d::Director::getInstance()->getWinSize().width / 2 - 150, cocos2d::Director::getInstance()->getWinSize().height / 2);
-        cocos2d::Vec2 pos2(cocos2d::Director::getInstance()->getWinSize().width / 2 + 150, cocos2d::Director::getInstance()->getWinSize().height / 2);
-
-        b2BodyDef bodyDef;
-        bodyDef.fixedRotation = true;
-        bodyDef.type = b2_dynamicBody;
-        body = world->CreateBody(&bodyDef);
-        body->SetGravityScale(2);
-        body->SetTransform(b2Vec2(pos.x / SCALE_RATIO, pos.y / SCALE_RATIO), body->GetAngle());
-
-        body2 = world->CreateBody(&bodyDef);
-        body2->SetGravityScale(2);
-        body2->SetTransform(b2Vec2(pos2.x / SCALE_RATIO, pos2.y / SCALE_RATIO), body->GetAngle());
-
-        cocos2d::Vec2 size = cocos2d::Vec2(50, 50);
-        b2FixtureDef fixDef;
-        b2CircleShape shape;
-        fixDef.restitution = 0.2f;
-        fixDef.shape = &shape;
-        shape.m_radius = 1;
-//        shape.SetAsBox(size.x / SCALE_RATIO, size.y / SCALE_RATIO);
-
-        fixDef.restitution = 1.0f;
-
-        body->CreateFixture(&fixDef);
-
-        body2->CreateFixture(&fixDef);
-    }
-
-    {
-        cocos2d::Vec2 pos(ScreenUtils::bottom().x,
-                          ScreenUtils::bottom().y);
-        b2BodyDef bodyDef;
-        bodyDef.fixedRotation = true;
-        bodyDef.type = b2_staticBody;
-
-        groundBody = world->CreateBody(&bodyDef);
-        groundBody->SetTransform(b2Vec2(pos.x / SCALE_RATIO, pos.y / SCALE_RATIO), groundBody->GetAngle());
-
-        cocos2d::Vec2 size = cocos2d::Vec2(ScreenUtils::getVisibleRect().size.width, 10);
-        b2FixtureDef fixDef;
-        b2PolygonShape shape;
-        fixDef.restitution = 0.2f;
-        fixDef.shape = &shape;
-        shape.SetAsBox(size.x / SCALE_RATIO, size.y / SCALE_RATIO);
-
-        groundBody->CreateFixture(&fixDef);
-    }
+    createBalls();
 
     scheduleUpdate();
 
@@ -112,22 +32,12 @@ bool GameLayer::init()
 
 void GameLayer::update(float dt)
 {
-    int positionIterations = 5;
-    int velocityIterations = 1;
-
-    world->Step(1.0 / 60.0, velocityIterations, positionIterations);
-
-    for (b2Body* body = world->GetBodyList(); body; body = body->GetNext())
-    {
-        if (body->GetUserData() != nullptr)
-        {
-        }
-    }
+    physicsManager->Update();
 }
 
 void GameLayer::RenderDebug()
 {
-    world->DrawDebugData();
+    physicsManager->renderDebug();
 }
 
 void GameLayer::draw(cocos2d::Renderer *renderer, const cocos2d::Mat4 &transform, uint32_t flags)
@@ -144,4 +54,71 @@ void GameLayer::draw(cocos2d::Renderer *renderer, const cocos2d::Mat4 &transform
     CHECK_GL_ERROR_DEBUG();
 
     director->popMatrix( cocos2d::MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW);
+}
+
+GameLayer::GameLayer()
+{
+
+}
+
+GameLayer::~GameLayer()
+{
+
+}
+
+void GameLayer::createBoundaries()
+{
+    const float offsetX = ScreenUtils::getVisibleRect().size.width * .05f;
+    const float offsetY = ScreenUtils::getVisibleRect().size.height * .05f;
+
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(0, 0); // bottom-left corner
+
+    b2Body* groundBody = physicsManager->GetWorld()->CreateBody(&groundBodyDef);
+    
+    // Define the ground box shape.
+    b2EdgeShape groundBox;
+
+    // bottom
+    groundBox.Set(b2Vec2((ScreenUtils::leftBottom().x + offsetX) / PTM_RATIO, (ScreenUtils::leftBottom().y + offsetY) / PTM_RATIO),
+                  b2Vec2((ScreenUtils::rightBottom().x - offsetX) / PTM_RATIO, (ScreenUtils::rightBottom().y + offsetY) / PTM_RATIO));
+    groundBody->CreateFixture(&groundBox,0);
+
+    // top
+    groundBox.Set(b2Vec2((ScreenUtils::leftTop().x + offsetX) / PTM_RATIO, (ScreenUtils::leftTop().y - offsetY) / PTM_RATIO),
+                  b2Vec2((ScreenUtils::rightTop().x - offsetX) / PTM_RATIO, (ScreenUtils::rightTop().y - offsetY) / PTM_RATIO));
+    groundBody->CreateFixture(&groundBox,0);
+
+    // left
+    groundBox.Set(b2Vec2((ScreenUtils::leftTop().x + offsetX) / PTM_RATIO, (ScreenUtils::leftTop().y - offsetY) / PTM_RATIO),
+                  b2Vec2((ScreenUtils::leftBottom().x + offsetX) / PTM_RATIO, (ScreenUtils::leftBottom().y + offsetY) / PTM_RATIO));
+    groundBody->CreateFixture(&groundBox,0);
+
+    // right
+    groundBox.Set(b2Vec2((ScreenUtils::rightBottom().x - offsetX) / PTM_RATIO, (ScreenUtils::rightBottom().y + offsetY) / PTM_RATIO),
+                  b2Vec2((ScreenUtils::rightTop().x - offsetX ) / PTM_RATIO, (ScreenUtils::rightTop().y - offsetY) / PTM_RATIO));
+    groundBody->CreateFixture(&groundBox,0);
+}
+
+void GameLayer::createBalls()
+{
+    const cocos2d::Size size = ScreenUtils::getVisibleRect().size;
+    const float startX = ScreenUtils::center().x - size.width * .1f;
+    float startY = ScreenUtils::center().y;
+    cocos2d::Vec2 pos = cocos2d::Vec2::ZERO;
+
+    //pyramid positioning logic
+    for (int i = 0; i < 5 ; i++)
+    {
+        for(int j = 0; j < i + 1; j++)
+        {
+            pos.x = startX - (BALL_RADIUS * 2) * i;
+            pos.y = startY - ((BALL_RADIUS * 2) * j);
+            Ball* ball = new Ball(*physicsManager->GetWorld(), pos);
+            addChild(ball);
+        }
+
+        startY = startY + (BALL_RADIUS);
+    }
+
 }
--- Classes/GameLayer.h
+++ Classes/GameLayer.h
@@ -6,36 +6,36 @@
 #define _GAMELAYER_H
 
 #include "cocos2d.h"
-#include "Utils/GLES-Render.h"
-#include "Box2D/Box2D.h"
+
+#define PTM_RATIO 32
 
 
-class GameLayer : public cocos2d::Scene{
+class PhysicsManager;
+
+class GameLayer : public cocos2d::Scene
+{
 
 public:
     static cocos2d::Scene* createScene();
 
     virtual bool init();
 
+    GameLayer();
+    ~GameLayer();
+
     // implement the "static create()" method manually
     CREATE_FUNC(GameLayer);
 
 private:
+    std::unique_ptr<PhysicsManager> physicsManager;
 
-    b2World* world = nullptr;
-    b2Body* body = nullptr;
-    b2Body* body2 = nullptr;
-    b2Body* groundBody = nullptr;
-    b2Vec2 gravity = b2Vec2(0, -9.8f);
-
-    GLESDebugDraw* debugDraw = nullptr;
-
+    void createBoundaries();
+    void createBalls();
 
     void update(float dt);
     void RenderDebug();
 
     void draw(cocos2d::Renderer *renderer, const cocos2d::Mat4& transform, uint32_t flags);
-
 
 };
 
