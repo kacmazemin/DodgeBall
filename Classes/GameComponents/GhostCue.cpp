ab1301de1ae7d2b9c1bb7918f16a3baa3e09d05e
mehmetemin <kacmazemin72@gmail.com>
2021-01-25 20:31:56 +0300

    feat: created GhostCue class

---
+++ Classes/GameComponents/GhostCue.cpp
@@ -0,0 +1,42 @@
+//
+// Created by emin on 24.01.2021.
+//
+
+#include "GhostCue.h"
+#include "../Utils/ScreenUtils.h"
+#include "Ball.h"
+
+GhostCue::GhostCue()
+{
+    cocos2d::Node::init();
+    autorelease();
+
+    createSprite();
+}
+
+void GhostCue::createSprite()
+{
+    sprite = cocos2d::Sprite::create("cue.png");
+
+    sprite->setOpacity(150);
+    sprite->setColor(cocos2d::Color3B::ORANGE);
+
+    ScreenUtils::fitW(sprite, BALL_RADIUS * 22);
+
+    sprite->setPositionX( sprite->getBoundingBox().size.width * .5f + (BALL_RADIUS * 3));
+
+    addChild(sprite);
+}
+
+cocos2d::Vec2 GhostCue::getPos() const
+{
+    return convertToWorldSpace(sprite->getPosition());
+}
+
+void GhostCue::changeVisibility(const bool isEnable)
+{
+    if(sprite)
+    {
+        sprite->setVisible(isEnable);
+    }
+}
---
+++ Classes/GameComponents/GhostCue.h
@@ -0,0 +1,33 @@
+//
+// Created by emin on 24.01.2021.
+//
+
+#ifndef _GHOSTCUE_H
+#define _GHOSTCUE_H
+
+#include <cocos/2d/CCNode.h>
+
+namespace cocos2d
+{
+    class Sprite;
+}
+
+class GhostCue final : public cocos2d::Node
+{
+public:
+    GhostCue();
+
+    cocos2d::Sprite* sprite = nullptr;
+
+    cocos2d::Vec2 getPos() const;
+
+    void changeVisibility(const bool isEnable);
+
+private:
+
+    void createSprite();
+
+};
+
+
+#endif //_GHOSTCUE_H
