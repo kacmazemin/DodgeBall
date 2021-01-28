f1848f74773b0100ee2a98cdbc348ebbb21950e6
mehmetemin <kacmazemin72@gmail.com>
2021-01-28 15:11:45 +0300

    feat: created cue panel to get hit power

---
+++ Classes/UI/CuePanel.cpp
@@ -0,0 +1,68 @@
+//
+// Created by emin on 28.01.2021.
+//
+
+#include "CuePanel.h"
+#include <cocos/2d/CCSprite.h>
+#include <cocos/ui/UILoadingBar.h>
+#include "../Utils/ScreenUtils.h"
+
+CuePanel::CuePanel(const cocos2d::Size& size) : size(size)
+{
+    cocos2d::Node::init();
+    autorelease();
+
+    createBackground();
+    createBar();
+
+    scheduleUpdate();
+}
+
+void CuePanel::createBackground()
+{
+    background = cocos2d::Sprite::create("textures/cue-panel-bar-bg.png");
+    ScreenUtils::fitH(background, size.height);
+
+    addChild(background);
+
+}
+
+void CuePanel::createBar()
+{
+    bar = cocos2d::ui::LoadingBar::create("textures/bar-gradient-fill-texture.png");
+    ScreenUtils::fitW(bar, size.height);
+    background->setRotation(90);
+
+    addChild(bar);
+}
+
+void CuePanel::update(float dt)
+{
+    if(bar->getPercent() >= 100.0f)
+    {
+        isHitMax = true;
+    }
+    else if(bar->getPercent() <= 0.f)
+    {
+        isHitMax = false;
+    }
+
+    percent = isHitMax ? percent - (dt * 150) :  percent + (dt * 150);
+
+    if(bar)
+    {
+        bar->setPercent(percent);
+    }
+}
+
+void CuePanel::changeActivity(const bool isActive)
+{
+    if(!isActive)
+    {
+        this->unscheduleUpdate();
+    }
+    else
+    {
+        this->scheduleUpdate();
+    }
+}
---
+++ Classes/UI/CuePanel.h
@@ -0,0 +1,42 @@
+//
+// Created by emin on 28.01.2021.
+//
+
+#ifndef CUEPANEL_H
+#define CUEPANEL_H
+
+#include <cocos/2d/CCNode.h>
+
+namespace cocos2d
+{
+    class Sprite;
+    class ClippingNode;
+    class Label;
+}
+namespace cocos2d::ui
+{
+    class LoadingBar;
+}
+
+class CuePanel final : public cocos2d::Node
+{
+public:
+    CuePanel(const cocos2d::Size& size);
+
+    void changeActivity(const bool isActive);
+
+    virtual void update(float dt) override;
+private:
+    float percent = 0;
+    bool isHitMax = false;
+    cocos2d::Size size = cocos2d::Size::ZERO;
+
+    cocos2d::Sprite* background = nullptr;
+    cocos2d::ui::LoadingBar* bar = nullptr;
+
+    void createBackground();
+    void createBar();
+};
+
+
+#endif //CUEPANEL_H
