e377562d0a8b26992eef57e1f05e2300b7abf4d2
mehmetemin <kacmazemin72@gmail.com>
2021-01-30 03:44:07 +0300

    feat: created CustomButton class

---
+++ Classes/UI/CustomButton.cpp
@@ -0,0 +1,71 @@
+//
+// Created by emin on 29.01.2021.
+//
+
+#include "CustomButton.h"
+#include "../Utils/ScreenUtils.h"
+#include <cocos/ui/UIButton.h>
+#include "AudioEngine.h"
+
+CustomButton::CustomButton(const std::string& path, const cocos2d::Size& size)
+: imagePath(path), size(size)
+{
+    cocos2d::Node::init();
+    autorelease();
+
+    createButton();
+}
+
+void CustomButton::createButton()
+{
+    initialNode = cocos2d::Node::create();
+    initialNode->setScale(1.f);
+    initialScale = initialNode->getScale();
+
+    addChild(initialNode);
+
+    mainButton = cocos2d::ui::Button::create(imagePath);
+    mainButton->setZoomScale(-mainButton->getZoomScale());
+    ScreenUtils::fitH(mainButton, size.height);
+
+    mainButton->addTouchEventListener([&](Ref* sender, cocos2d::ui::Widget::TouchEventType type){
+        switch (type)
+        {
+            case cocos2d::ui::Widget::TouchEventType::BEGAN:
+                playClickEffect();
+                break;
+            case cocos2d::ui::Widget::TouchEventType::MOVED:
+                break;
+            case cocos2d::ui::Widget::TouchEventType::ENDED:
+
+                if(function)
+                {
+                    function();
+                }
+                break;
+            default:
+                break;
+        }
+    });
+
+    initialNode->addChild(mainButton);
+}
+
+void CustomButton::setOnTap(const std::function<void()>& func)
+{
+    function = func;
+}
+
+
+cocos2d::Rect CustomButton::getBoundingBox() const
+{
+    return cocos2d::utils::getCascadeBoundingBox(initialNode);
+}
+
+void CustomButton::playClickEffect()
+{
+    if(isSoundEffectEnable)
+    {
+        cocos2d::experimental::AudioEngine::play2d("Sounds/basic-click.wav");
+    }
+}
---
+++ Classes/UI/CustomButton.h
@@ -0,0 +1,45 @@
+//
+// Created by emin on 29.01.2021.
+//
+
+#ifndef CUSTOMBUTTON_H
+#define CUSTOMBUTTON_H
+
+#include <cocos/2d/CCNode.h>
+
+namespace cocos2d::ui
+{
+    class Button;
+}
+
+class CustomButton : public cocos2d::Node
+{
+
+public:
+    CustomButton(const std::string& path, const cocos2d::Size& size);
+
+    void setOnTap(const std::function<void()>& func);
+
+    virtual cocos2d::Rect getBoundingBox() const ;
+
+private:
+
+    bool isSoundEffectEnable = true;
+    float initialScale = .0f;
+    std::string imagePath = "";
+    cocos2d::Size size = cocos2d::Size::ZERO;
+
+    cocos2d::Node* initialNode = nullptr;
+    cocos2d::ui::Button* mainButton = nullptr;
+
+    std::function<void()> function;
+
+    void createButton();
+    void playClickEffect();
+
+
+
+};
+
+
+#endif //CUSTOMBUTTON_H
