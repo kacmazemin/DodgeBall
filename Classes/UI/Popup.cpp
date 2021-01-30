405c4239a86b4edd3697ec071ae51950b618d0eb
mehmetemin <kacmazemin72@gmail.com>
2021-01-30 21:56:15 +0300

    feat: added helper function

--- Classes/UI/Popup.cpp
+++ Classes/UI/Popup.cpp
@@ -12,28 +12,30 @@ Popup::Popup()
     cocos2d::Node::init();
     autorelease();
 
-    createBackground();
+    createLayout();
     createTouchListener();
 
     createCloseButton();
 }
 
-void Popup::createBackground()
+void Popup::createLayout()
 {
     contentNode = cocos2d::Node::create();
     addChild(contentNode);
 
-    backgroundSprite = cocos2d::Sprite::create("textures/popup-layout.png");
-    ScreenUtils::fitWH(backgroundSprite, ScreenUtils::getVisibleRect().size.width,
+    layoutSprite = cocos2d::Sprite::create("textures/popup-layout.png");
+    ScreenUtils::fitWH(layoutSprite, ScreenUtils::getVisibleRect().size.width,
                                          ScreenUtils::getVisibleRect().size.height);
 
-    contentNode->addChild(backgroundSprite);
+    contentNode->addChild(layoutSprite);
 }
 
 void Popup::createTouchListener()
 {
     auto listener = cocos2d::EventListenerTouchOneByOne::create();
+
     listener->setSwallowTouches(true);
+
     listener->onTouchBegan = [=](cocos2d::Touch* touch, cocos2d::Event* event)
     {
         return true;
@@ -43,15 +45,35 @@ void Popup::createTouchListener()
 
 void Popup::createCloseButton()
 {
-    auto* closeButton = new CustomButton("textures/close-button.png", backgroundSprite->getBoundingBox().size * .1f);
+    closeButton = new CustomButton("textures/close-button.png", layoutSprite->getBoundingBox().size * .1f);
 
-    closeButton->setPositionX(backgroundSprite->getBoundingBox().getMaxX() - closeButton->getBoundingBox().size.width * .75f);
-    closeButton->setPositionY(backgroundSprite->getBoundingBox().getMaxY() - closeButton->getBoundingBox().size.height * .75f);
+    closeButton->setPositionX(layoutSprite->getBoundingBox().getMaxX() - closeButton->getBoundingBox().size.width * .75f);
+    closeButton->setPositionY(layoutSprite->getBoundingBox().getMaxY() - closeButton->getBoundingBox().size.height * .75f);
 
     closeButton->setOnTap([=]()
     {
-        this->removeFromParentAndCleanup(true);
+        closePopup();
     });
 
-    contentNode->addChild(closeButton);
+    contentNode->addChild(closeButton, 2);
+}
+
+void Popup::fitCloseButton(const cocos2d::Rect& rect)
+{
+    if(closeButton)
+    {
+        closeButton->setPositionX(rect.getMaxX() - closeButton->getBoundingBox().size.width * .9f);
+        closeButton->setPositionY(rect.getMaxY() - closeButton->getBoundingBox().size.height * .9f);
+    }
+
+}
+
+void Popup::closePopup()
+{
+    this->removeFromParentAndCleanup(true);
+}
+
+void Popup::changeCloseButtonVisibility(const bool isVisible)
+{
+    closeButton->setVisible(isVisible);
 }
--- Classes/UI/Popup.h
+++ Classes/UI/Popup.h
@@ -12,19 +12,26 @@ namespace cocos2d
     class Sprite;
 }
 
+class CustomButton;
+
 class Popup : public cocos2d::Node
 {
 public:
     Popup();
 
 protected:
-    cocos2d::Sprite* backgroundSprite = nullptr;
+    cocos2d::Sprite* layoutSprite = nullptr;
     cocos2d::Node* contentNode = nullptr;
 
+    void fitCloseButton(const cocos2d::Rect& rect);
+    void closePopup();
+    void changeCloseButtonVisibility(const bool isVisible);
 
 private:
 
-    void createBackground();
+    CustomButton* closeButton = nullptr;
+
+    void createLayout();
     void createTouchListener();
     void createCloseButton();
 };
