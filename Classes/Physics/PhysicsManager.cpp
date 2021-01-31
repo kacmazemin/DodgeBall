065d821c00da5b317531f8f1e3d4ae6f387b94d8
mehmetemin <kacmazemin72@gmail.com>
2021-01-31 19:10:48 +0300

    fix: fixed deconstructor crash

--- Classes/Physics/PhysicsManager.cpp
+++ Classes/Physics/PhysicsManager.cpp
@@ -27,13 +27,13 @@ PhysicsManager::PhysicsManager()
 
 PhysicsManager::~PhysicsManager()
 {
-    b2Body* body = world->GetBodyList();
-
-    for (int i = 0; i < world->GetBodyCount(); i++)
-    {
-        b2Body* existBody = &body[i];
-        if (existBody) world->DestroyBody(existBody);
-    }
+//    b2Body* body = world->GetBodyList();
+//
+//    for (int i = 0; i < world->GetBodyCount(); i++)
+//    {
+//        b2Body* existBody = &body[i];
+//        if (existBody) world->DestroyBody(existBody);
+//    }
 
     delete world;
     world = nullptr;
